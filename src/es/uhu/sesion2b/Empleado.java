0a9df155e91c05e8faf1d365a46973c92de31c1e
jccastrocc <jccastroc.develop@gmail.com>
2024-09-30 17:40:41 +0200

    Primera implementacion Empleado

---
+++ src/es/uhu/sesion2b/Empleado.java
@@ -0,0 +1,45 @@
+package es.uhu.sesion2b;
+
+public class Empleado {
+
+	public enum TipoEmpleado {
+		VENDEDOR, ENCARGADO;
+	}
+
+	float salarioBase;
+	float primas;
+	float horasExtras;
+
+	public float calculoNominaBruta(TipoEmpleado tipo, float ventasMes, float horasExta) {
+
+		if (tipo == TipoEmpleado.ENCARGADO) {
+			salarioBase = 2500;
+		} else if (tipo == TipoEmpleado.VENDEDOR) {
+			salarioBase = 2000;
+		}
+
+		if (ventasMes >= 2500) {
+			primas = 200;
+		} else if (ventasMes >= 1000) {
+			primas = 100;
+		}
+
+		this.horasExtras = horasExtras * 30;
+		
+		return salarioBase + primas + this.horasExtras;
+		
+	}
+
+	public float calculoNominaNeta(float nominaBruta) {
+		
+		float retencion = 0;
+		if(nominaBruta >2500) {
+			retencion = 0.18f;
+		}else if(nominaBruta>2000) {
+			retencion = 0.15f;
+		}
+	
+		return nominaBruta*(1-retencion);
+	}
+
+}
